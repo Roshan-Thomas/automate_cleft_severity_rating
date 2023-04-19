325c29b1b4a3d8ea3a956371677abfb7a046561b
Roshan-Thomas <roshan.sunil.thomas@gmail.com>
2023-04-19 13:32:58 +0300

    Pipline is completed - Basic code for it is done which includes generating severity score from original image

--- automatic_rating_pipeline/pipeline.py
+++ automatic_rating_pipeline/pipeline.py
@@ -1,24 +1,30 @@
 from pixelwise_subtraction.pixelwise_subtraction import pixelwise_subtraction_gradio
 from image_inpainting.ui import image_inpainting
 from cnn_classifier.ui import detect_nasolabial_area
+import cv2
+import imutils
+import numpy as np
 
 def automatic_rating_pipeline(input_image):
+    input_image = imutils.resize(input_image, width=512)
+
     # classifier
     masked_nasolabial_area_image, _ = detect_nasolabial_area(input_image)
 
     print("[INFO] Identified Nasolabial Area.")
 
     # stable diffusion
-    normalized_image = image_inpainting(masked_nasolabial_area_image,
-                                        sampling_method="Euler a",
-                                        sampling_steps=20,
-                                        width=512,
-                                        height=512,
-                                        cfg_scale=7,
-                                        denoising_strength=0.75,
-                                        seed=92548865)
+    normalized_image = image_inpainting(input_image=input_image,
+                                        mask_image=cv2.imread('images/mask.jpeg'),
+                                        cfg_scale=7.5,
+                                        seed=92548865
+                                        )
 
     print("[INFO] Completed Image Inpainting.")
+
+    np_image = np.array(normalized_image)
+    normalized_image_opencv = cv2.cvtColor(np_image, cv2.COLOR_RGB2BGR)
+    normalized_image = imutils.resize(normalized_image_opencv, width=512)
 
     # pixel wise subtraction
     _, _, severity_rating = pixelwise_subtraction_gradio(input_image, normalized_image)
--- image_inpainting/ui.py
+++ image_inpainting/ui.py
@@ -2,11 +2,45 @@ import gradio as gr
 import torch
 from diffusers import StableDiffusionInpaintPipeline
 import os
+import imutils
+from PIL import Image
+import cv2
+
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
+def image_inpainting(input_image, mask_image, cfg_scale, seed):
+    device = "cuda"
+    if torch.cuda.is_available():
+        generator = torch.Generator('cuda').manual_seed(seed)
+    else:
+        generator = torch.Generator().manual_seed(seed)
 
-def image_inpainting(dict, sampling_method, sampling_steps, width, height, cfg_scale, denoising_strength, seed):
+    color_converted_input = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+    input_image = Image.fromarray(color_converted_input)
+
+    mask_image = imutils.resize(mask_image, width=512)
+    color_converted_mask = cv2.cvtColor(mask_image, cv2.COLOR_BGR2RGB)
+    mask_image = Image.fromarray(color_converted_mask)
+
+    model_path = "runwayml/stable-diffusion-inpainting"
+
+    pipe = StableDiffusionInpaintPipeline.from_pretrained(
+        model_path,
+        torch_dtype=torch.float16,
+    ).to(device)
+
+    output_image = pipe(
+        prompt=" ",
+        image=input_image,
+        mask_image=mask_image,
+        guidance_scale=cfg_scale,
+        generator=generator,
+    ).images
+
+    return (output_image[0])
+
+def image_inpainting_gradio(dict, sampling_method, sampling_steps, width, height, cfg_scale, denoising_strength, seed):
     device = "cuda"
 
     seed = 92548865
@@ -27,7 +61,7 @@ def image_inpainting(dict, sampling_method, sampling_steps, width, height, cfg_s
     mask_image = dict['mask'].convert("RGB").resize((512, 512))
 
     output_image = pipe(
-        prompt="",
+        prompt=" ",
         image=input_image,
         mask_image=mask_image,
         guidance_scale=cfg_scale,
@@ -66,7 +100,7 @@ def image_inpainting_ui():
 
 
     submit_button.click(
-        fn=image_inpainting,
+        fn=image_inpainting_gradio,
         inputs=[input_image, sampling_method, sampling_steps, width, heigth, cfg_scale, denoising_strength, seed],
         outputs=output_image,
     )
